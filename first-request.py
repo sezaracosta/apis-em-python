1a2c6ea72e92ca575b88f0b35b36c978119ba003
sezaracosta <sezara41@gmail.com>
2024-10-10 10:41:02 -0300

    foram adiconados novos arquivos

--- first-request.py
+++ first-request.py
@@ -5,3 +5,7 @@ url = "https://www.google.com"
 answer = requests.get(url)
 
 print(answer)
+print(answer.text)
+
+with open ('page.google.html', 'w') as file:
+    file.write(answer.text)
---
+++ generating-request.py
@@ -0,0 +1,20 @@
+import requests
+
+url = 'https://httpbin.org/post'
+
+data = {
+    "meus_dados": [1, 2, 3],
+    "pessoa": {
+        "nome":"richard",
+        "professor": True
+    }
+}
+params = {
+    'dataInicio': '2024-01-01', 
+    'dataFim': '2024-02-01',
+}
+
+answer = requests.post(url,json=data,params=params)
+
+print(answer.request.url)
+#print(answer.json())
