0a6dded184700c392aa60547d823304674d64e75
sezaracosta <sezara41@gmail.com>
2024-10-14 17:13:03 -0300

    corrigindo a url municipio

--- api.py
+++ api.py
@@ -1,7 +1,20 @@
+from pprint import pprint
+import requests
 
-url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/{municipio}"
+
+url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios"
+
 
 params = {
-
-    
 }
+
+resposta = requests.get(url,params=params)
+
+try:
+    resposta.raise_for_status()
+except requests.HTTPError as e:
+    print(f'Erro no request: {e}')
+    resultado = None
+else:
+    resultado = resposta.json()
+    pprint (resultado)
