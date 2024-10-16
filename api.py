3c915e28ac45cd7ed7f780b928c3ef98015ef41c
sezaracosta <sezara41@gmail.com>
2024-10-16 08:14:04 -0300

    modificando o arquivo api.py

--- api.py
+++ api.py
@@ -1,20 +1,47 @@
 from pprint import pprint
 import requests
 
+def pegar_ids_estado():
+    url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
 
-url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios"
+    params = {
+        'view':'nivelado',
+    }
+    dados_estados = fazer_request(url=url, params=params)
+    dict_estado = {}
+    for dados in dados_estados:
+        id_estado = dados['UF-id']
+        nome_estado = dados ['UF-nome']
+        dict_estado[id_estado] = nome_estado
+    return dict_estado
+
+def frequencia_nome_por_estado(nome):
+    url = f"https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}"
+
+    params = {
+        'groupBy': 'UF',
+    }
+    dados_frequencia = fazer_request(url=url, params=params)
+    dict_frequencia = {}
+    for dados in dados_frequencia:
+        print(dados)
 
 
-params = {
-}
+def fazer_request(url, params=None):
+    reposta = requests.get(url, params=params)
+    try:
+        reposta.raise_for_status()
+    except requests.HTTPError as e:
+        print(f'erro no request: {e}')
+        resultado = None
+    else:
+        resultado = reposta.json()
+    return resultado
 
-resposta = requests.get(url,params=params)
 
-try:
-    resposta.raise_for_status()
-except requests.HTTPError as e:
-    print(f'Erro no request: {e}')
-    resultado = None
-else:
-    resultado = resposta.json()
-    pprint (resultado)
+def main ():
+    dict_estados = pegar_ids_estado()
+    pprint(dict_estados)
+
+if __name__ == '__main__':
+    main()
